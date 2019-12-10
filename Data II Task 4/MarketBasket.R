d480423b41a3e175698dc77cda22a3739eb27367
Damsa Ioana <damsaioana17@gmail.com>
2019-12-10 12:15:16 +0100

    plot started

--- Data II Task 4/MarketBasket.R
+++ Data II Task 4/MarketBasket.R
@@ -6,11 +6,14 @@ library(arulesViz)
 
 
 #Upload data and set seed + set number of displayed significant
-Transactions<- read.transactions("ElectronidexTransactions2017.csv")
+Transactions<- read.transactions("ElectronidexTransactions2017.csv",format = "basket", 
+                                 sep = ",", rm.duplicates = TRUE)
+Transactions
 set.seed(123)
 options(digits = 2)
 productCategory <- read.csv("ProductCategories.csv")
 productCategory<- as.data.frame(productCategory)
+
 
 #Inspect dataset
 inspect(Transactions)
@@ -19,22 +22,41 @@ size(Transactions) # Number of items per transaction
 LIST(Transactions) # Lists the transactions by conversion
 itemLabels(Transactions)# To see the item labels
 summary(Transactions)
-head(Transactions@itemInfo)
+
 
 #Visualize dataset
 itemFrequencyPlot(Transactions, support=relative)
 image(Transactions)
 image(sample(Transactions)) #add more things to this badboy
+ncol(Transactions)
+
+### Compare Blackwell and Electronidex product offering
+
+# Prepare Blackwell product data
+blackwellNew <- read.csv("blackwellNew.csv")
+blackwellNew <- blackwellNew[c(2,3)]
+blackwellOld <- read.csv("blackwellOld.csv")
+blackwellOld <- blackwellOld[c(1,2)]
+
+blackwellProductTypes <- rbind(blackwellOld, blackwellNew)
+blackwellProductTypes <- data.frame(blackwellProductTypes$ProductType, blackwellProductTypes$ProductNum)
+View(blackwellProductTypes)
+
+blackwellProductTypes$Company <- "Blackwell"
+colnames(blackwellProductTypes)[1] <- "ProductType"
+colnames(blackwellProductTypes)[2] <- "Product"
+
 
 # Adding the labels
-Transactions@itemInfo <- productCategory
+Transactions@itemInfo$category <- productCategory$Category
 
 
 # TOP 10 most frequent products
 itemFrequencyPlot(Transactions, topN = 10, col = rainbow(4), type="absolute")
+head(Transactions@itemInfo)
 
 #Association rules
-rules <- apriori(Transactions, parameter=list(support=0.001, confidence=0.5))
+rules <- apriori(Transactions, parameter=list(support=0.001, confidence=0.8))
 rules
 
 
